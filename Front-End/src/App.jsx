51577410941797db736724e1eb3be0881141a893
leopromus <leopromus@gmail.com>
2025-11-01 18:31:46 +0200

    Add Stacks, Portfolio, Testimonials, and Contact components to App layout

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -3,12 +3,20 @@ import Description from "./components/Body";
 
 import "./App.css";
 import Footer from "./components/Footer";
+import Portfolio from "./portfolio";
+import Stacks from "./Stacks";
+import Contact from "./Contact";
+import Testimonials from "./Testimonials";
 
 function App() {
   return (
     <>
       <Navigations />
       <Description />
+      <Stacks/>
+      <Portfolio/>
+       <Testimonials />
+      <Contact/>
       <Footer />
     </>
   );

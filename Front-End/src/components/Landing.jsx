36016e481e90788481dcd8aef35459bead02e90d
leopromus <leopromus@gmail.com>
2025-11-01 18:39:11 +0200

    Refactor Navigations component to use React Scroll for smooth navigation and update logo display

--- Front-End/src/components/Landing.jsx
+++ Front-End/src/components/Landing.jsx
@@ -1,32 +1,42 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../assets/promisleo-logo-transparent.png";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { Link } from "react-scroll";
 
 function Navigations() {
   return (
     <>
       <nav className="navbar-custom d-flex align-items-center justify-content-between px-5 py-3 m">
-        <h2 className="logo ms-5">Kamana</h2>
+        <div className="logo ms-1">
+  <img src={logo} alt="PromisLeo Logo" style={{ height: "50px" }} />
+</div>
+
 
         <ul className="nav-links d-flex list-unstyled m-0">
           <li>
-            <a href="#home" className="nav-link-custom">
-              Home
-            </a>
+            <Link to="#" smooth={true} duration={500} className="nav-link-custom">
+              Overview
+            </Link>
           </li>
           <li>
-            <a href="#contact" className="nav-link-custom">
-              Contact me
-            </a>
+            <Link to="#" smooth={true} duration={500} className="nav-link-custom">
+              Profile
+            </Link>
           </li>
           <li>
-            <a href="#about" className="nav-link-custom">
-              About-us
-            </a>
+            <Link to="stacks" smooth={true} duration={500} className="nav-link-custom">
+              Expertise
+            </Link>
           </li>
           <li>
-            <a href="#about" className="nav-link-custom">
-              Projects
-            </a>
+            <Link to="portfolio" smooth={true} duration={500} className="nav-link-custom">
+              Works
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500} className="nav-link-custom">
+              Get in Touch
+            </Link>
           </li>
         </ul>
 
@@ -37,4 +47,5 @@ function Navigations() {
     </>
   );
 }
+
 export default Navigations;
